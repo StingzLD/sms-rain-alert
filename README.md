288e5c690c48cfea2ce94a7eb19f4f547eabbb1e
Corey Hastings <stingzld@gmail.com>
2023-08-06 13:49:01 -0500

    Added SMS functionality and converted to envirnomental variables

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # SMS Rain Alert
 
-Use the OpenWeather API to gather weather forecast data and send an SMS message in the morning when rain is in the forecast for the day
+Use the OpenWeather API to gather weather forecast data and send an SMS message in the 
+morning when rain is in the forecast for the day
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import requests
+import os
+from twilio.rest import Client
 
 # Website to find your Latitude and Longitude:
 #   https://www.latlong.net/
@@ -8,12 +10,14 @@ import requests
 #   https://openweathermap.org/weather-conditions#Weather-Condition-Codes-2
 
 owm_endpoint = "https://api.openweathermap.org/data/3.0/onecall"
-api_key = "87b297d28dca77fa42dc81c9ea35dcca"
+owm_api_key = os.environ['OWM_API_KEY']
+twilio_account_sid = os.environ['TWILIO_ACCOUNT_SID']
+twilio_auth_token = os.environ['TWILIO_AUTH_TOKEN']
 
 weather_params = {
-    "lat": "45.151",
-    "lon": "-98.413",
-    "appid": api_key,
+    "lat": os.environ['MY_HOME_LATITUDE'],
+    "lon": os.environ['MY_HOME_LONGITUDE'],
+    "appid": owm_api_key,
     "units": "imperial",
     "exclude": "current,minutely,daily"
 }
@@ -23,8 +27,23 @@ response.raise_for_status()
 weather_data = response.json()
 hourly_data = weather_data['hourly'][:12]  # Next 12 hours
 
+will_rain = False
+
 for hour in hourly_data:
     condition_code = hour['weather'][0]['id']
     if int(condition_code) < 600:  # If it is raining
-        print("Bring an umbrella")
+        will_rain = True
         break
+
+if will_rain:
+    client = Client(twilio_account_sid, twilio_auth_token)
+
+    message = client.messages \
+        .create(
+            body="🌧️ Rain is in today's forecast. 🌧️\n☔ Bring an umbrella. ☔",
+            from_=os.environ['MY_TWILIO_NUMBER'],
+            to=os.environ['MY_PHONE_NUMBER']
+        )
+    print(message.status)
+else:
+    print("No rain in the forecast")
