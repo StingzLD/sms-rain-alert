304d036794f041291ce848c5e219a0230fd418d6
Corey Hastings <stingzld@gmail.com>
2023-08-24 11:07:27 -0500

    Updated to include probablilty of precipitation

--- main.py
+++ main.py
@@ -9,20 +9,22 @@ from twilio.rest import Client
 # Weather condition codes:
 #   https://openweathermap.org/weather-conditions#Weather-Condition-Codes-2
 
-owm_endpoint = "https://api.openweathermap.org/data/3.0/onecall"
-owm_api_key = os.environ['OWM_API_KEY']
-twilio_account_sid = os.environ['TWILIO_ACCOUNT_SID']
-twilio_auth_token = os.environ['TWILIO_AUTH_TOKEN']
+OWM_ENDPOINT = "https://api.openweathermap.org/data/3.0/onecall"
+OWM_API_KEY = os.environ['OWM_API_KEY']
+TWILIO_ACCOUNT_SID = os.environ['TWILIO_ACCOUNT_SID']
+TWILIO_AUTH_TOKEN = os.environ['TWILIO_AUTH_TOKEN']
+TWILIO_PHONE_NUMBER = os.environ['TWILIO_PHONE_NUMBER']
+TWILIO_VERIFIED_NUMBER = os.environ['TWILIO_VERIFIED_NUMBER']
 
 weather_params = {
     "lat": os.environ['MY_HOME_LATITUDE'],
     "lon": os.environ['MY_HOME_LONGITUDE'],
-    "appid": owm_api_key,
+    "appid": OWM_API_KEY,
     "units": "imperial",
     "exclude": "current,minutely,daily"
 }
 
-response = requests.get(owm_endpoint, params=weather_params)
+response = requests.get(OWM_ENDPOINT, params=weather_params)
 response.raise_for_status()
 weather_data = response.json()
 hourly_data = weather_data['hourly'][:12]  # Next 12 hours
@@ -31,18 +33,20 @@ will_rain = False
 
 for hour in hourly_data:
     condition_code = hour['weather'][0]['id']
-    if int(condition_code) < 600:  # If it is raining
+    probability = hour['pop']
+
+    if int(condition_code) < 600 and probability > 0.5:
         will_rain = True
         break
 
 if will_rain:
-    client = Client(twilio_account_sid, twilio_auth_token)
+    client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
     message = client.messages \
         .create(
             body="🌧️ Rain is in today's forecast. 🌧️\n☔ Bring an umbrella. ☔",
-            from_=os.environ['MY_TWILIO_NUMBER'],
-            to=os.environ['MY_PHONE_NUMBER']
+            from_=TWILIO_PHONE_NUMBER,
+            to=TWILIO_VERIFIED_NUMBER
         )
     print(message.status)
 else:
